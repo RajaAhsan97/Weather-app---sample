b8a6d59a4e1aa349404b68bd35435c6588678a13
Raja Muhammad Ahsan <ahsanrajaq056@gmail.com>
2026-01-13 21:07:07 +0500

    Add files via upload

---
+++ api_client/openweather_client.py
@@ -0,0 +1,32 @@
+import requests
+from dotenv import load_dotenv
+import os
+#from config import settings 
+#from config.settings import API_KEY, BASE_URL
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+def fetch_current_weather(city):
+    url = f"{BASE_URL}/weather"
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    return requests.get(url, params=params).json()
+
+
+def fetch_forecast_weather(city):
+    url = f"{BASE_URL}/forecast"
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    return requests.get(url, params=params).json()
